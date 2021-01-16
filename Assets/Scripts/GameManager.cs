44272a31cda613e795269a6c6b1c9340dc8a4f27
Asra/2000 <foreverasra@gmail.com>
2021-01-16 10:29:48 +0530

    add initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public GameObject Panel;
+    public GameObject India;
+    public Data data;
+    public GameObject quiz;
+    public GameObject button;
+    private string ans;
+
+    public AnimationPlay player;
+
+    public string giveAns () {
+        return ans;
+    }
+
+    public void OnButtonClick () {
+        Panel.SetActive (!Panel.activeSelf);
+    }
+
+    private void Start () {
+        if (data.isQuestionEmpty ()) {
+            KeyValuePair<string, string> pair = data.getQuestion ();
+            data.ChangeQuestionText (pair.Key);
+            ans = pair.Value;
+            player.getAnswer (ans);
+        }
+    }
+
+    public void ActivateQuiz () {
+        quiz.SetActive (true);
+        button.SetActive (false);
+        India.SetActive(true);
+    }
+
+    public void InactivateQuiz(){
+        quiz.SetActive (false);
+        button.SetActive (true);
+        India.SetActive(false);
+    }
+
+}
